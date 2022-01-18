1d04f8694b94c01db53fbd9ec78cee47d66f951e
Vaishnavi-5G <96188244+Vaishnavi-5G@users.noreply.github.com>
2022-01-18 18:53:20 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -14,32 +14,17 @@ document.getElementById("player_question").innerHTML="Question Turn -"+player1_n
 document.getElementById("player_answer").innerHTML="Answer Turn -"+player2_name;
 
 function send() {
-get_word= document.getElementById("word").value;
-word= get_word.toLowerCase();
-console.log("word in lowerCase=" +word);
+number1=document.getElementById("number1").value;
+number2=document.getElementById("number2").value;
+actual_answer=parseInt(number1) * parseInt(number2);
 
-charAt1=word.charAt(1);
-console.log(charAt1);
-
-length_divide_2=Math.floor(word.length/2);
-charAt2=word.charAt(length_divide_2);
-console.log(charAt2);
-
-legth_minus_1= word.length-1;
-charAt3= word.charAt(legth_minus_1);
-console.log(charAt3);
-
-remove_charAt1=word.replace(charAt1, "_");
-remove_charAt2= remove_charAt1.replace(charAt2, "_");
-remove_charAt3= remove_charAt2.replace(charAt3, "_");
-console.log(remove_charAt3);
-question_word = "<h4 id='word_display'> Q. "+remove_charAt3+"</h4>";
-    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+question_number = "<h4>" + number1+"x"+ number2 + "</h4>";
+    input_box = "<br>Answer : <input type='number' id='input_check_box'>";
     check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
-    row =  question_word + input_box + check_button ; 
+    row =  question_number + input_box + check_button ; 
     document.getElementById("output").innerHTML = row;
-document.getElementById("word").value = "";
-
+document.getElementById("number1").value = "";
+document.getElementById("number2").value = "";
 
 
 }
@@ -47,9 +32,9 @@ question_turn="player1";
 answer_turn="player2";
 function check() {
 get_answer=document.getElementById("input_check_box").value ; 
-answer=get_answer.toLowerCase();
-console.log("answer in lower case-"+answer);
-if (answer==word) 
+
+
+if (get_answer==actual_answer) 
 {
 if (answer_turn=="player1")  
 {
